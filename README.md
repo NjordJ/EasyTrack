2ff8e7a7bf47d4562a1569edc51563e030548512
Ildar Biglov <nj0rd3841@gmail.com>
2022-06-24 17:01:17 +0500

    initial project

---
+++ README.md
@@ -0,0 +1,6 @@
+# easy_track
+
+Project on tracking the amount of money spent.
+
+Design: https://symu.co/freebies/templates-4/trackizer/
+
---
+++ android/app/src/main/kotlin/com/example/easy_track/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.easy_track
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
